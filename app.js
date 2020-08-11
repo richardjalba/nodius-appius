20aa50688b44ab2e5c27f17f671df28950ef75b1
Richard <richardjalba@gmail.com>
2020-08-10 17:37:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const connectDB = require("./config/db");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+// Passport config
+require("./config/passport")(passport);
+
+connectDB();
+
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// 40:12 Passport Middleware
+//
+//
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
